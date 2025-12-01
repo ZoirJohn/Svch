2421a11e9759f0a13459b5e602bb19f73994d8c2
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-12-01 21:29:53 +0500

    Move Hero component markup to Hero.tsx for better organization

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,18 @@
 export default function Hero() {
 	return (
-		<section className="">
-			
+		<section className="mt-3 px-3">
+			<div className="mx-auto container">
+				<div className="relative rounded-3xl min-h-180 overflow-hidden banner select-none">
+					<img
+						src="/assets/img/banner.png"
+						alt="Banner"
+						className="z-1 absolute w-full"
+					/>
+					<div className="group z-2 relative flex flex-col justify-end items-center hover:bg-blue-light/45 min-h-[inherit] text-white transition-all *:transition-all *:duration-500 gap-10">
+						<p className="opacity-0 group-hover:opacity-100 group-hover:-translate-y-40 text-xl">Delete us after the wedding.</p>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 }
--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,20 +1,9 @@
+import Hero from "components/Hero/Hero";
+
 export default function Home() {
 	return (
 		<>
-			<section className="mt-3 px-3">
-				<div className="mx-auto container">
-					<div className="relative rounded-3xl min-h-180 overflow-hidden banner select-none">
-						<img
-							src="/assets/img/banner.png"
-							alt="Banner"
-							className="z-1 absolute w-full"
-						/>
-						<div className="group z-2 relative flex flex-col justify-end items-center hover:bg-blue-light/45 min-h-[inherit] text-white transition-all *:transition-all *:duration-500 gap-10">
-							<p className="opacity-0 group-hover:opacity-100 group-hover:-translate-y-40 text-xl">Delete us after the wedding.</p>
-						</div>
-					</div>
-				</div>
-			</section>
+			<Hero />
 		</>
 	);
 }
