3d3a6f5aacdee3d74beea7d52141dd490e512b28
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-12-28 18:51:42 +0500

    Refactor Login component; update input fields for email and password with improved validation and error handling, and enhance form structure for better readability

--- pages/Auth/Login.tsx
+++ pages/Auth/Login.tsx
@@ -2,7 +2,19 @@ import { useForm, type SubmitHandler } from "react-hook-form";
 import type { Field, LoginForm } from "shared/types";
 import AuthOTPButton from "widgets/AuthOTPButton";
 
-const FIELDS: Field<LoginForm>[] = [{ autoComplete: "name", inputType: "text", htmlFor: "fullName", labelText: "Full name", validation: { required: true, pattern: /^[A-Z][a-z]+ [A-Z][a-z]+$/ } }];
+const FIELDS: Field<LoginForm>[] = [
+	{
+		autoComplete: "email",
+		inputType: "email",
+		htmlFor: "email",
+		labelText: "Email",
+		validation: {
+			required: true,
+			pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+		},
+	},
+	{ autoComplete: "password", inputType: "password", labelText: "Password", htmlFor: "password", validation: { required: true } },
+];
 
 export default function SignUp() {
 	const {
@@ -15,19 +27,17 @@ export default function SignUp() {
 	};
 	return (
 		<main className="flex justify-center items-center shrink-0 grow">
-			<form onSubmit={handleSubmit(onSubmit)} className="[&>div]:relative flex flex-col items-center shadow [&>div]:mb-6 p-8 border border-blue-light rounded-lg [&>div]:w-full md:w-96">
+			<form onSubmit={handleSubmit(onSubmit)} className="relative flex flex-col items-center shadow [&>label]:mb-6 p-8 border border-blue-light rounded-lg [&>label]:w-full md:w-96">
 				<h1 className="mb-4 text-4xl!">Login</h1>
 				{FIELDS.map(({ autoComplete, inputType, htmlFor, labelText, validation }) => {
 					return (
-						<div>
-							<label htmlFor={htmlFor} className="bg-primary form-label">
-								{labelText}
-							</label>
-							<input {...register(htmlFor, { ...validation })} type={inputType} id={htmlFor} autoComplete={autoComplete} />
-							{errors.fullName && <span className="input-error">Firstname Lastname</span>}
-						</div>
+						<label htmlFor={htmlFor} className="bg-primary form-label">
+							{labelText}
+							<input {...register(htmlFor, validation)} type={inputType} id={htmlFor} autoComplete={autoComplete} />
+						</label>
 					);
 				})}
+				{(errors.email || errors.password) && <span className="input-error bottom-1/2 translate-y-9.5 text-center">Incorrect email or password</span>}
 
 				<button type="submit" className="mb-8 ml-auto px-4 py-2 rounded-lg button-primary">
 					Login
