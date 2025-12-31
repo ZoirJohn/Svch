d53b60621bf3eb469dbbeba6c98a5aa9f0b46aa6
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-01-01 01:14:49 +0500

    Applied correct logic to the routing: The home page of the dashboard will always be the matches page

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("../pages/Home/Home.tsx"), route("/auth/sign-in", "../pages/Auth/Login.tsx"), route("/auth/sign-up", "../pages/Auth/Signup.tsx"), route("/dashboard", "../pages/Dashboard/Dashboard.tsx")] satisfies RouteConfig;
+export default [
+	index("../pages/Home/Home.tsx"),
+	route("/auth/sign-in", "../pages/Auth/Login.tsx"),
+	route("/auth/sign-up", "../pages/Auth/Signup.tsx"),
+	// Dashboard
+	route("/dashboard", "../pages/Dashboard/Dashboard.tsx", [
+		index('../pages/Dashboard/index.tsx'),
+		route("matches", "../pages/Matches/Matches.tsx"), //
+		route("profile", "../pages/Profile/Profile.tsx"), //
+		route("settings", "../pages/Settings/Settings.tsx"),
+	]),
+] satisfies RouteConfig;
--- pages/Dashboard/Dashboard.tsx
+++ pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router";
 import Sidebar from "widgets/Sidebar";
 
 export default function Dashboard() {
 	return (
 		<main className="main">
 			<Sidebar />
+			<Outlet />
 		</main>
 	);
 }
